c4524e365b47cf5a83d1333d5be4408d39eae54f
deekep <MVSein@stud.kantiana.ru>
2022-04-20 17:40:03 +0200

    second commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
-#include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+class Foo{
+public:
+    Foo(int j) {
+        i = new int[j];
+    }
+    virtual ~Foo() { //добавлено virtual
+        delete[] i; // delete -> delete[]
+    }
+private:
+    int* i;
+};
+
+class Bar: public Foo{ // Foo -> public Foo
+public:
+    Bar(int j) : Foo(j) { // добавлено  : Foo(j)
+        i = new char[j];
+    }
+    virtual ~Bar() { //добавлено virtual
+        delete[] i; // delete -> delete[]
+    }
+private:
+    char* i;
+};
+
+int main(){ // void main() -> int main()
+    Foo* f = new Foo(100);
+    Foo* b = new Bar(200);
+    *f = *b;
+
+    delete f;
+    delete b;
 }
+
