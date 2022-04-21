4e50918ac0503854583e23528729fc31a1f681b7
deekep <MVSein@stud.kantiana.ru>
2022-04-21 11:10:13 +0200

    last version

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 
+#include <iostream>
 class Foo{
 public:
     Foo(int j) {
         i = new int[j];
     }
+
+    Foo(const Foo& object) : i(object.i){   //добавлен конструктор копирования
+    }
+
+    Foo& operator=(const Foo& object){  //добавлен оператор копирования
+        return *this;
+    }
+
     virtual ~Foo() { //добавлено virtual
-        delete[] i; // delete -> delete[] // вот тут ошибка
+        delete[] i;
     }
 protected: // private -> protected
     int* i;
@@ -16,6 +25,7 @@ public:
     Bar(int j) : Foo(j) { // добавлено  : Foo(j)
         i = new char[j];
     }
+
     virtual ~Bar() { //добавлено virtual
         delete[] i; // delete -> delete[]
     }
@@ -25,7 +35,7 @@ protected: // private -> protected
 
 int main(){ // void main() -> int main()
     Foo* f = new Foo(100);
-    Foo* b = new Bar(200);
+    Foo* b = new Bar(100);
     *f = *b;
 
     delete f;
